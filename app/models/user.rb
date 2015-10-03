dd1626f1a3970bfed5b4ad818e765d8e1b2e1e7f
Benjamim Sonntag <benjamimsonntag@gmail.com>
2015-10-03 16:20:44 +0100

    Adds balance method to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :coffees, :money, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def balance
+    money - (coffees * 40)
+  end
+
   private
 
   def default_values
